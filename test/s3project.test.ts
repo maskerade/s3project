4729813205e54f89a6c4673cb92bea154c0409bf
Stefan De Figueiredo <stefanfigueiredo@rideways.com>
2021-12-17 17:01:06 +0000

    Initial commit

---
+++ test/s3project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3Project from '../lib/s3project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3Project.S3ProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
